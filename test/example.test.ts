45727688d4003453f8b4c36a78cc259b19cc6309
Mehmet <hi@productdevbook.com>
2023-10-05 07:18:25 +0300

    feat: upgrade structure (#18)
    * feat: upgrade struct
    
    * chore: update renovate config
    
    * fix: eslint

--- test/example.test.ts
+++ test/example.test.ts
@@ -3,6 +3,6 @@ import { describe, it } from 'vitest'
 // The two tests marked with concurrent will be run in parallel
 describe('suite', () => {
   it('serial test', async () => { /* ... */ })
-  it.concurrent('concurrent test 1', async ({ expect }) => { /* ... */ })
-  it.concurrent('concurrent test 2', async ({ expect }) => { /* ... */ })
+  it.concurrent('concurrent test 1', async () => { /* ... */ })
+  it.concurrent('concurrent test 2', async () => { /* ... */ })
 })
